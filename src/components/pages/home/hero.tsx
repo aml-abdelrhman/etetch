b68a16f677ca1c041d06793ed30de53609659450
OsamaHIma <osamahima018@gmail.com>
2026-02-04 13:57:55 +0200

    push

--- src/components/pages/home/hero.tsx
+++ src/components/pages/home/hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const HomeHeroSection = () => {
   return (
-    <div className="bg-[url('/hero-bg.svg')] bg-cover bg-center bg-no-repeat min-h-[70svh] w-full">
+    <div className="bg-[url(/hero-bg.svg)] bg-cover bg-center bg-no-repeat min-h-[80svh] w-full">
       <div className="container text-center">HomeHeroSection</div>
     </div>
   );
