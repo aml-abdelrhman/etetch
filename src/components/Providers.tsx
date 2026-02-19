3cfb2db547c0bdb7671ec03879dc798855dde3fa
OsamaHIma <osamahima018@gmail.com>
2026-02-19 15:49:06 +0200

    push

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -8,6 +8,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { getQueryClient } from "@/app/get-query-client";
 import { DirectionProvider } from "./ui/direction";
 import moment from "moment";
+import NextTopLoader from "nextjs-toploader";
 
 moment.locale("ar");
 
@@ -22,6 +23,19 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
         dir={locale === "ar" ? "rtl" : "ltr"}
         direction={locale === "ar" ? "rtl" : "ltr"}
       >
+        <NextTopLoader
+          color="#683c21"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3.1}
+          crawl
+          showSpinner
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #eadec6,0 0 5px #eadec6"
+          template='<div class="bar" role="bar"><div class="peg"></div></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+          zIndex={1600}
+        />
         <Suspense>
           <Toaster
             closeButton
